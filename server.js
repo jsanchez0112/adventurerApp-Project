7e65e161da084da40f81b59eb6079a29c27da524
jsanchez0112 <edsaja12@gmail.com>
2022-05-27 14:21:45 -0500

    event changes

--- server.js
+++ server.js
@@ -46,5 +46,3 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
 
 
-
-//add in more code over the weekend. Adding this in to ensure the github repo is working.
