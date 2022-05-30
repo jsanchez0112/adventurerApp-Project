41926f4ddb81306a646c8b77bc7424468322c172
jsanchez0112 <edsaja12@gmail.com>
2022-05-30 18:22:28 -0500

    new user hashing

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,19 @@ const userRouter = express.Router();
 const User = require('../models/users')
 
 
+//Registration Page 
+userRouter.get('/new' , (req,res) => {
+      res.render('users/new.ejs' , {
+            currentUser: req.sessions.currentUser
+      })});
 
-
-
-
+//Create Registration Router
+userRouter.post('/' , (req,res) => {
+      //hash the user password and pass it to the database for safekeeping.
+      req.body.password = bccrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+      User.create(req.body, (error, createdUser) => {
+            res.redirect('/')
+      })});
 
 
 //Export User Router
