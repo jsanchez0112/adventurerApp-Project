52e0d0b91d0b06bc4e54e7064264917f57896974
jsanchez0112 <edsaja12@gmail.com>
2022-06-02 09:30:44 -0500

    gallery adjustments

--- controllers/gallery.js
+++ controllers/gallery.js
@@ -3,6 +3,14 @@ const express = require('express');
 const galleryRouter = express.Router();
 const Gallery = require('../models/gallery')
 
+//New
+galleryRouter.get('/gallery' , (req,res) => {
+      res.render('gallery/gallery.ejs')
+      })
+      galleryRouter.post('/' , (req,res) => {
+            Gallery.create(req.body, (err, createdGallery) => {
+                  res.redirect('/gallery')
+            })});
 
 galleryRouter.get('/:id/edit', (req,res) => {
       Gallery.findById(req.params.id, (err, foundGallery) => {
